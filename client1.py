3865734c15d982377da447f2d1374eb7bfe6423a
Scuf <maks201022051980@gmail.com>
2026-03-19 23:57:52 +0400

    Update client1.py

--- client1.py
+++ client1.py
@@ -7,6 +7,17 @@ user_name_2 = "user_2"
 
 
 def connected():
+    def receive_messages(sock):
+        while True:
+            try:
+                data = sock.recv(1024)
+                if not data:
+                    break
+                print(f"\nСобеседник: {data.decode()}")
+                print("Вы: ", end="", flush=True)
+            except:
+                break
+
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         client.connect((socket.gethostname(), 12345))
@@ -14,16 +25,20 @@ def connected():
         print("Сервер не запущен!")
         return
 
+    thread = threading.Thread(target=receive_messages, args=(client,))
+    thread.daemon = True
+    thread.start()
+
     auth_data = f"{user_name}:{password}"
     client.send(auth_data.encode())
-
-    
+    import time
+    time.sleep(1000)
+    client.send(user_name.encode())
     while True:
         message = input("Вы: ")
-        if message.lower() == 'exit':
-            break
-        if message.strip():
-            client.send(message.encode())
+        message = f"{user_name_2},{message}"
+        client.send(message.encode())
+
 
     client.close()
 
